60100df12240c5598fdb76a048fbc48c982b6b9e
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-24 00:20:09 +0545

    User Routes

--- backend/user/src/routes/user.ts
+++ backend/user/src/routes/user.ts
@@ -1,8 +1,21 @@
 import express from "express";
-import { loginUser } from "../controllers/user.js";
+import {
+  getAllUsers,
+  getAUser,
+  loginUser,
+  myProfile,
+  updateName,
+  verifyUser,
+} from "../controllers/user.js";
+import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
-console.log("User routes loaded");
+
 router.post("/login", loginUser);
+router.post("/verify", verifyUser);
+router.get("/me", isAuth, myProfile);
+router.get("/user/all", isAuth, getAllUsers);
+router.get("/user/:id", getAUser);
+router.post("/update/user", isAuth, updateName);
 
 export default router;
