e2576252200856c2c471f25ff889387494db7542
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-26 19:17:23 +0545

    chore: import dependencies

--- backend/user/src/config/db.ts
+++ backend/user/src/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose, { Mongoose } from "mongoose";
 
 const connectDb = async () => {
-  const url = process.env.MONGO_URI;
+  const url = process.env.MONGO_URI!;
 
   if (!url) {
     throw new Error("MONGO_URI is not defined in the environment variables");
