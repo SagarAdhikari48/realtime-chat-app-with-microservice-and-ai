82ea27656641ca7d47080a3ad8677e8c2cef193f
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-27 17:38:02 +0545

    feat: suspense and loading to handle production issue

--- frontend/src/app/verify/page.tsx
+++ frontend/src/app/verify/page.tsx
@@ -1,12 +1,15 @@
-"use client";
-
+import Loading from "@/components/Loading";
 import VerifyOtp from "@/components/VerifyOtp";
+import { Suspense } from "react";
 
 const VerifyPage = () => {
   return (
-    <div>
+    //this will not throw error during development phase but
+    // when we tried to buld in th production it will throw error so we need suspense to handle this issue
+    //suspense is given with loading component to handle the loading state of the component which is being lazy loaded
+    <Suspense fallback={<Loading />}>
       <VerifyOtp />
-    </div>
+    </Suspense>
   );
 };
 
