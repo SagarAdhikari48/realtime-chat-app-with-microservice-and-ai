f1897c2f5c0ac5d0a246f6669a5e2dcbad2b2c9c
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-30 16:48:03 +0545

    feat: create chat sidebar

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,12 +1,49 @@
 "use client";
+import ChatSidebar from "@/components/ChatSidebar";
 import Loading from "@/components/Loading";
-import { useAppData } from "@/context/AppContext";
+import { useAppData, User } from "@/context/AppContext";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+export interface Message {
+  //These should match exact spell as backend has
+  _id: string;
+  chatId: string;
+  senderId: string;
+  text?: string;
+  image?: {
+    url: string;
+    publicId: string;
+  };
+  messageType: "text" | "image";
+  seen: boolean;
+  seenAt?: string;
+  createdAt: string;
+}
 
 const ChatApp = () => {
   //if not loggedin then it will redirect to login page so we use useeffect here.
-  const { loading, isAuth } = useAppData();
+  const {
+    loading,
+    isAuth,
+    logoutUser,
+    chats,
+    user: loggedInUser,
+    users,
+    fetchChats,
+    setChats,
+  } = useAppData();
+
+  const [selectedUser, setSelectedUser] = useState<string | null>(null);
+  const [message, setMessage] = useState("");
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [messages, setMessages] = useState<Message[] | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [showAllUsers, setShowAllUsers] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
+  const [typingTimeOut, setTypingTimeOut] = useState<NodeJS.Timeout | null>(
+    null,
+  );
 
   const router = useRouter();
 
@@ -16,9 +53,26 @@ const ChatApp = () => {
     }
   }, [isAuth, router, loading]);
 
+  const handleLogout = async () => logoutUser();
+
   if (loading) return <Loading />;
-  
-  return <div>Chat App</div>;
+
+  return (
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+      <ChatSidebar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        shopwAllUsers={showAllUsers}
+        setShowAllUsers={setShowAllUsers}
+        users={users}
+        loggedInUser={loggedInUser}
+        chats={chats}
+        selectedUser={selectedUser}
+        setSelectedUser={setSelectedUser}
+        handleLogout={handleLogout}
+      />
+    </div>
+  );
 };
 
 export default ChatApp;
