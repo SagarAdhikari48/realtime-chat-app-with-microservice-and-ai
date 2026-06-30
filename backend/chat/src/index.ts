60446ce8aa529f50991711eca05ecea2401924f1
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-30 12:59:58 +0545

    chore: cors

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import chatRoutes from "./routes/chat.js";
 import morgan from "morgan";
+import cors from "cors";
 
 dotenv.config();
 
@@ -13,10 +14,10 @@ const app = express();
 const port = process.env.PORT!;
 
 app.use(express.json());
+app.use(cors());
 app.use(morgan("dev"));
 
 app.use("/api/v1", chatRoutes);
- 
 
 app.listen(port, () => {
   console.log(`Server is running on the port ${port}`);
