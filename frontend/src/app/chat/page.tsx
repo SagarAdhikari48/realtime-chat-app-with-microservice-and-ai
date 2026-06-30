57c3f7f775e49f48d3ea53d70964deaa86c3af3e
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-30 12:24:38 +0545

    fix: redirect to chat or login based on active session

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+"use client";
+import Loading from "@/components/Loading";
+import { useAppData } from "@/context/AppContext";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const ChatApp = () => {
+  //if not loggedin then it will redirect to login page so we use useeffect here.
+  const { loading, isAuth } = useAppData();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isAuth && !loading) {
+      router.push("/login");
+    }
+  }, [isAuth, router, loading]);
+
+  if (loading) return <Loading />;
+  
   return <div>Chat App</div>;
 };
 
