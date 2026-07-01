6a333d31eb5838ad595187c01f019c3461f26a36
Sagar Adhikari <sagradhkr48@gmail.com>
2026-07-01 17:14:14 +0545

    feat: Socket Configuration

--- backend/chat/src/config/socket.ts
+++ backend/chat/src/config/socket.ts
@@ -1,9 +1,31 @@
 import { Server, Socket } from "socket.io";
 import http from "http";
 import express from "express";
+import { error } from "console";
 
 const app = express();
 
 const server = http.createServer(app);
 
-export { app, server };
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const userSocketMap: Record<string, string> = {};
+
+io.on("connection", (socket: Socket) => {
+  console.log("User connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+
+  socket.on("connect_error", () => {
+    console.log("Socket Connection Error", error);
+  });
+});
+
+export { app, server, io };
